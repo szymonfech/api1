e4bd121af41e47eb2090f1b3dbeab735a96d2c45
Szymon <szymon.fechner@gmail.com>
2022-08-15 18:33:04 +0200

    added styles and coments to JS file

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ window.onload = function () {
   let button = document.getElementById("button");
 
   button.addEventListener("click", function () {
+    // fetch Api data with input value
     fetch("https://api.tvmaze.com/search/shows?q=" + input.value)
       .then((response) => response.json())
       .then((data) => generateDataResults(data));
@@ -12,26 +13,30 @@ window.onload = function () {
     document.querySelector(".container").innerHTML = "";
     console.log(data);
     for (let i = 0; i < data.length; i++) {
+      //Creating elemets
       let newCard = document.createElement("div");
-      newCard.classList.add("card");
       let cardDescription = document.createElement("div");
-      cardDescription.classList.add("card-description");
       let cardName = document.createElement("span");
-      cardName.classList.add("name");
-      cardName.innerHTML = data[i].show.name;
       let cardCategory = document.createElement("span");
-      cardCategory.classList.add("category");
-      cardCategory.innerHTML = data[i].show.genres;
       let cardImg = document.createElement("img");
+      let cardInfo = document.createElement("p");
+      // Adding classes
+      newCard.classList.add("card");
+      cardDescription.classList.add("card-description");
+      cardName.classList.add("name");
+      cardCategory.classList.add("category");
+      cardInfo.classList.add("summary");
+      // Adding values from API
+      cardName.innerHTML = "Tytuł: " + data[i].show.name;
+      cardCategory.innerHTML = "Kategoria: " + data[i].show.genres;
+      cardInfo.innerHTML = data[i].show.summary;
+      // Catching lack of img in api
       if (data[i].show.image) {
         cardImg.setAttribute("src", data[i].show.image.medium);
       } else {
         cardImg.setAttribute("src", "pngegg.png");
       }
-      let cardInfo = document.createElement("p");
-      cardInfo.classList.add("summary");
-      cardInfo.innerHTML = data[i].show.summary;
-      console.log(cardName);
+      // Adding created and adjusted elements to existing DOM
       cardDescription.append(cardImg, cardName, cardCategory, cardInfo);
       newCard.appendChild(cardDescription);
       document.querySelector(".container").appendChild(newCard);
